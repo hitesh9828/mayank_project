d139f52f5cee9e0dd7830798516a59c3fdbeab07
hitesh9828 <hiteshjaisinghani11@gmail.com>
2016-03-31 22:08:06 -0700

    first commit

---
+++ Mayank/supply_chain_management/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/supply_chain_management/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/supply_chain_management/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/supply_chain_management/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/supply_chain_management/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\\wamp\\www\\supply_chain_management/admin/');
+define('DIR_SYSTEM', 'C:\\wamp\\www\\supply_chain_management/system/');
+define('DIR_DATABASE', 'C:\\wamp\\www\\supply_chain_management/system/database/');
+define('DIR_LANGUAGE', 'C:\\wamp\\www\\supply_chain_management/admin/language/');
+define('DIR_TEMPLATE', 'C:\\wamp\\www\\supply_chain_management/admin/view/template/');
+define('DIR_CONFIG', 'C:\\wamp\\www\\supply_chain_management/system/config/');
+define('DIR_IMAGE', 'C:\\wamp\\www\\supply_chain_management/image/');
+define('DIR_CACHE', 'C:\\wamp\\www\\supply_chain_management/system/cache/');
+define('DIR_DOWNLOAD', 'C:\\wamp\\www\\supply_chain_management/download/');
+define('DIR_LOGS', 'C:\\wamp\\www\\supply_chain_management/system/logs/');
+define('DIR_CATALOG', 'C:\\wamp\\www\\supply_chain_management/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'supply_chain_management');
+define('DB_PREFIX', '');
+?>
---
+++ Mayank/supply_chain_management/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/supply_chain_management/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/supply_chain_management/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\\wamp\\www\\supply_chain_management/catalog/');
+define('DIR_SYSTEM', 'C:\\wamp\\www\\supply_chain_management/system/');
+define('DIR_DATABASE', 'C:\\wamp\\www\\supply_chain_management/system/database/');
+define('DIR_LANGUAGE', 'C:\\wamp\\www\\supply_chain_management/catalog/language/');
+define('DIR_TEMPLATE', 'C:\\wamp\\www\\supply_chain_management/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\\wamp\\www\\supply_chain_management/system/config/');
+define('DIR_IMAGE', 'C:\\wamp\\www\\supply_chain_management/image/');
+define('DIR_CACHE', 'C:\\wamp\\www\\supply_chain_management/system/cache/');
+define('DIR_DOWNLOAD', 'C:\\wamp\\www\\supply_chain_management/download/');
+define('DIR_LOGS', 'C:\\wamp\\www\\supply_chain_management/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'supply_chain_management');
+define('DB_PREFIX', '');
+?>
